df1cb2ac4d36b299555c33eea8d9ae6b182041cb
takenncs <116774376+Takennncs@users.noreply.github.com>
2026-03-01 15:52:10 +0200

    Update version_check.lua

--- version_check.lua
+++ version_check.lua
@@ -1,21 +1,16 @@
 local currentVersion = '1.0.0'
-local repo = 'Takennncs/takenncs-lib'
 
 CreateThread(function()
     Wait(5000)
-    
-    PerformHttpRequest(('https://raw.githubusercontent.com/%s/main/version.txt'):format(repo), 
+    print(('^2[takenncs-lib]^7 Version %s'):format(currentVersion))
+    PerformHttpRequest('https://raw.githubusercontent.com/Takennncs/takenncs-lib/main/version.txt', 
         function(err, text, headers)
             if err == 200 then
                 local latestVersion = text:match('(%d+%.%d+%.%d+)')
                 if latestVersion and latestVersion ~= currentVersion then
-                    print(('^3[takenncs-lib]^7 New version available: %s (current: %s)'):format(latestVersion, currentVersion))
-                    print(('^3[takenncs-lib]^7 Download: https://github.com/%s/releases/latest'):format(repo))
-                else
-                    print(('^2[takenncs-lib]^7 Version %s is up to date'):format(currentVersion))
+                    print(('^3[takenncs-lib]^7 New version available: %s'):format(latestVersion))
                 end
             else
-                print('^1[takenncs-lib]^7 Failed to check for updates')
-            end
+          end
         end, 'GET')
 end)
